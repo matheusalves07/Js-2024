96ade7eff3d45f1f2d2f66775b4bbd6168a7a325
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2024-12-04 19:19:31 -0300

    at

--- stripe.js
+++ stripe.js
@@ -2,20 +2,24 @@ paymentRequest.on('paymentmethod', function(ev) {
     stripe.confirmCardPayment(
       '{{CLIENT_SECRET}}',
       {payment_method: ev.paymentMethod.id},
+
       {handleActions: false}
     ).then(function(confirmResult) {
       if (confirmResult.error) {
         ev.complete('fail');
       } else {
         ev.complete('success');
+        
         if (confirmResult.paymentIntent.status === "requires_action") {
           stripe.confirmCardPayment(clientSecret).then(
             function(result) {
               if (result.error) {
-                // The payment failed    --  ask your customer for a new payment method.
+                // The payment failed    --  ask your customer for a new payment method.  ténica de pagmento falido //
+                //@result = @ev(Text = AnimationTimeline)// 
               } else {
                 
-                // Pagamento com sucesso. 
+                // Pagamento com sucesso.  
+                
               }
             }
           );
